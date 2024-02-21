fcd214427f0051cf7d1b4054c1371ad66967afc5
Sionwage <None>
2024-02-21 10:35:25 -0600

    Found the bug causing the agilentE4418B to not work, line 207 overwrote the read function

--- pymeasure/instruments/agilent/agilentE4418B.py
+++ pymeasure/instruments/agilent/agilentE4418B.py
@@ -29,36 +29,36 @@ from time import time as now
 import numpy as np
 from pyvisa import VisaIOError
 
-from pymeasure.instruments import Instrument #, Channel
+from pymeasure.instruments import Instrument, Channel
 from pymeasure.instruments.validators import strict_discrete_set, truncated_range, strict_range
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
-# class PowerMeterChannel(Channel):
-#     """Channel implimentation for the Agilent E4418B Power Meter"""
+class PowerMeterChannel(Channel):
+    """Channel implimentation for the Agilent E4418B Power Meter"""
 
-#     frequency = Channel.control('SENS{ch}:FREQ?',
-#                                 'SENS{ch}:FREQ %e',
-#                                 """Control the frequency the power meter corrects its
-#                                 measurement for in Hz. Value range can be changed based
-#                                 on the power head used.
+    frequency = Channel.control('SENS{ch}:FREQ?',
+                                'SENS{ch}:FREQ %e',
+                                """Control the frequency the power meter corrects its
+                                measurement for in Hz. Value range can be changed based
+                                on the power head used.
 
-#                                 Type: :code:`float`
+                                Type: :code:`float`
 
-#                                 .. code-block:: python
+                                .. code-block:: python
 
-#                                     # set the frequency to 1.21GHz
-#                                     instr.frequency = 1.21e9
+                                    # set the frequency to 1.21GHz
+                                    instr.frequency = 1.21e9
 
-#                                     if instr.frequency == 10e6:
-#                                         pass
+                                    if instr.frequency == 10e6:
+                                        pass
 
-#                                 """,
-#                                 values=[10e6, 18e9],
-#                                 validator=strict_range,
-#                                 dynamic=True,
-#                                 cast=float)
+                                """,
+                                values=[10e6, 18e9],
+                                validator=strict_range,
+                                dynamic=True,
+                                cast=float)
 
 
 class AgilentE4418B(Instrument):
@@ -79,33 +79,33 @@ class AgilentE4418B(Instrument):
     def __init__(
         self,
         adapter=None,
-        name="Agilent E4418B Power Meter",
+        name=None,
         **kwargs,
     ):
-        super().__init__(adapter=adapter, name=name, includeSCPI=False, **kwargs)
+        super().__init__(adapter=adapter, name=name, includeSCPI=True, **kwargs)
 
-    #     self._manu = ""
-    #     self._model = ""
-    #     self._fw = ""
-    #     self._sn = ""
-    #     self._options = ""
-    #     self.adapter = adapter
+        self._manu = ""
+        self._model = ""
+        self._fw = ""
+        self._sn = ""
+        self._options = ""
+        self.adapter = adapter
 
 
-    #     if name is None:
-    #         # written this way to pass 'test_all_instruments.py' while allowing the
-    #         # *IDN? to populate the name of the VNA
-    #         try:
-    #             self._manu, self._model, _, self._fw = self.id
-    #         except ValueError:
-    #             self._manu = "Agilent"
-    #             self._model = "E4418B"
-    #         self._desc = "Power Meter"
-    #         name = self.name = f"{self._manu} {self._model} {self._desc}"
-    #     else:
-    #         self.name = name
+        # if name is None:
+        #     # written this way to pass 'test_all_instruments.py' while allowing the
+        #     # *IDN? to populate the name of the VNA
+        #     try:
+        #         self._manu, self._model, _, self._fw = self.id
+        #     except ValueError:
+        #         self._manu = "Agilent"
+        #         self._model = "E4418B"
+        #     self._desc = "Power Meter"
+        #     name = self.name = f"{self._manu} {self._model} {self._desc}"
+        # else:
+        #     self.name = name
 
-    # channels = Instrument.MultiChannelCreator(PowerMeterChannel, [1, 2])
+    channels = Instrument.MultiChannelCreator(PowerMeterChannel, [1, 2])
 
     # def ask(self, command, query_delay=None):
     #     self.adapter.write(command)
@@ -125,107 +125,107 @@ class AgilentE4418B(Instrument):
         cast=str,
     )
 
-    @property
-    def manu(self):
-        """Get the manufacturer of the instrument."""
-        if self._manu == "":
-            self._manu, self._model, _, self._fw = self.id
-        return self._manu
+    # @property
+    # def manu(self):
+    #     """Get the manufacturer of the instrument."""
+    #     if self._manu == "":
+    #         self._manu, self._model, _, self._fw = self.id
+    #     return self._manu
 
-    @property
-    def model(self):
-        """Get the model of the instrument."""
-        if self._model == "":
-            self._manu, self._model, _, self._fw = self.id
-        return self._model
+    # @property
+    # def model(self):
+    #     """Get the model of the instrument."""
+    #     if self._model == "":
+    #         self._manu, self._model, _, self._fw = self.id
+    #     return self._model
 
-    @property
-    def fw(self):
-        """Get the firmware of the instrument."""
-        if self._fw == "":
-            self._manu, self._model, _, self._fw = self.id
-        return self._fw
+    # @property
+    # def fw(self):
+    #     """Get the firmware of the instrument."""
+    #     if self._fw == "":
+    #         self._manu, self._model, _, self._fw = self.id
+    #     return self._fw
 
     # channels = Instrument.MultiChannelCreator(PowerMeterChannel, list(range(1,3)))
 
     # ch_1 = Instrument.ChannelCreator(PowerMeterChannel, "1")
 
 
-    frequency = Instrument.control('SENS1:FREQ?',
-                                   'SENS1:FREQ %e',
-                                   """Control the frequency the power meter corrects its
-                                    measurement for in Hz. Value range can be changed based
-                                    on the power head used.
+    # frequency = Instrument.control('SENS1:FREQ?',
+    #                                'SENS1:FREQ %e',
+    #                                """Control the frequency the power meter corrects its
+    #                                 measurement for in Hz. Value range can be changed based
+    #                                 on the power head used.
 
-                                    Type: :code:`float`
+    #                                 Type: :code:`float`
 
-                                    .. code-block:: python
+    #                                 .. code-block:: python
 
-                                        # set the frequency to 1.21GHz 
-                                        instr.frequency = 1.21e9
+    #                                     # set the frequency to 1.21GHz 
+    #                                     instr.frequency = 1.21e9
 
-                                        if instr.frequency == 10e6:
-                                            pass
+    #                                     if instr.frequency == 10e6:
+    #                                         pass
 
-                                    """,
-                                   values=[10e6, 18e9],
-                                   validator=strict_range,
-                                   dynamic=True,
-                                   cast=float)
+    #                                 """,
+    #                                values=[10e6, 18e9],
+    #                                validator=strict_range,
+    #                                dynamic=True,
+    #                                cast=float)
 
-    offset = Instrument.control('CORR:LOSS2?',
-                                'CORR:LOSS2 %e',
-                                """
-                                Control the offset applied (in dB) to the power sensor measurement
-                                to account for cable and other losses to the power meter. (float) 
-                                """,
-                                cast=float,
-                                validator=strict_range,
-                                values=[-100,100])
+    # offset = Instrument.control('CORR:LOSS2?',
+    #                             'CORR:LOSS2 %e',
+    #                             """
+    #                             Control the offset applied (in dB) to the power sensor measurement
+    #                             to account for cable and other losses to the power meter. (float) 
+    #                             """,
+    #                             cast=float,
+    #                             validator=strict_range,
+    #                             values=[-100,100])
 
-    enable_offset = Instrument.control('CORR:LOSS2:STAT?',
-                                       'CORR:LOSS2:STAT %s',
-                                       """
-                                       Control the offset applied (in dB) to the power sensor measurement
-                                       to account for cable and other losses to the power meter. (float) 
-                                       """,
-                                       cast=bool,
-                                       values={True: 'ON',
-                                               False: 'OFF'}
-                                       )
+    # enable_offset = Instrument.control('CORR:LOSS2:STAT?',
+    #                                    'CORR:LOSS2:STAT %s',
+    #                                    """
+    #                                    Control the offset applied (in dB) to the power sensor measurement
+    #                                    to account for cable and other losses to the power meter. (float) 
+    #                                    """,
+    #                                    cast=bool,
+    #                                    values={True: 'ON',
+    #                                            False: 'OFF'}
+    #                                    )
 
-    average_count = None
-    averaging_enable = None
-    auto_averaging = None
-    filter = None
-    filter_enabled = None
-    filter_mode = None
-    trigger_auto_delay = None
-    trigger_source = None
-    trigger_immediate = None
-    trigger_continuous = None
-    read = None
-    measure = None
-    resolution = None
-    zero = None
-    calibrate = None
-    power_reference_enabled = None # Enable/Disable
-    step_determination_enabled = None
-    relative_offset = None
-    relative_offset_enabled = None
-    upper_window_unit = Instrument.control('UNIT1:POW?',
-                                           'UNIT1:POW %s',
-                                           """
-                                           Control the measurement units for the upper window. (string)
-                                           """,
-                                           cast=str,
+    # average_count = None
+    # averaging_enable = None
+    # auto_averaging = None
+    # filter = None
+    # filter_enabled = None
+    # filter_mode = None
+    # trigger_auto_delay = None
+    # trigger_source = None
+    # trigger_immediate = None
+    # trigger_continuous = None
+    # read = None
+    # measure = None
+    # resolution = None
+    # zero = None
+    # calibrate = None
+    # power_reference_enabled = None # Enable/Disable
+    # step_determination_enabled = None
+    # relative_offset = None
+    # relative_offset_enabled = None
+    # upper_window_unit = Instrument.control('UNIT1:POW?',
+    #                                        'UNIT1:POW %s',
+    #                                        """
+    #                                        Control the measurement units for the upper window. (string)
+    #                                        """,
+    #                                        cast=str,
 
-                                    )
-    lower_window_unit = Instrument.control('UNIT2:POW?',
-                                           'UNIT2:POW %s',
-                                           """
-                                           Control the measurement units for the lower window. (string)
-                                           """,
-                                           cast=str,
+    #                                 )
+    # lower_window_unit = Instrument.control('UNIT2:POW?',
+    #                                        'UNIT2:POW %s',
+    #                                        """
+    #                                        Control the measurement units for the lower window. (string)
+    #                                        """,
+    #                                        cast=str,
 
-                                    )
+    #                                 )
