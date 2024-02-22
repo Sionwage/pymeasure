36993c4d20f83c778b161e341cf1f300f353fe7e
Sionwage <None>
2024-02-22 15:33:05 -0600

    Added power measurement and moved channel properties needed to channel class.

--- pymeasure/instruments/agilent/agilentE4418B.py
+++ pymeasure/instruments/agilent/agilentE4418B.py
@@ -23,64 +23,105 @@
 #
 
 import logging
-from time import sleep
-from time import time as now
-
-import numpy as np
-from pyvisa import VisaIOError
 
 from pymeasure.instruments import Channel, Instrument
-from pymeasure.instruments.validators import (
-    strict_discrete_set,
-    strict_range,
-    truncated_range,
-)
+from pymeasure.instruments.validators import strict_range
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
+
 class PowerMeterChannel(Channel):
     """Channel implimentation for the Agilent E4418B Power Meter"""
 
-    frequency = Channel.control('SENS{ch}:FREQ?',
-                                'SENS{ch}:FREQ %e',
-                                """Control the frequency the power meter corrects its
-                                measurement for in Hz. Value range can be changed based
-                                on the power head used.
+    frequency = Channel.control(
+        "SENS{ch}:FREQ?",
+        "SENS{ch}:FREQ %e",
+        """Control the frequency the power meter corrects its
+        measurement for in Hz. Value range can be changed based
+        on the power head used.
 
-                                Type: :code:`float`
+        Type: :code:`float`
 
-                                .. code-block:: python
+        .. code-block:: python
 
-                                    # set the frequency to 1.21GHz
-                                    instr.frequency = 1.21e9
+            # set the frequency to 1.21GHz
+            instr.frequency = 1.21e9
 
-                                    if instr.frequency == 10e6:
-                                        pass
+            if instr.frequency == 10e6:
+                pass
 
-                                """,
-                                values=[10e6, 18e9],
-                                validator=strict_range,
-                                dynamic=True,
-                                cast=float)
+        """,
+        values=[10e6, 18e9],
+        validator=strict_range,
+        dynamic=True,
+        cast=float,
+    )
 
-    offset = Channel.control('SENS{ch}:CORR:LOSS2?',
-                                'SENS{ch}:CORR:LOSS2 %e',
-                                """
-                                Control the offset subtracted (in dB) from the power sensor measurement
-                                to account for cable and other losses to the power meter. (float) 
-                                """,
-                                cast=float,)
-                                # validator=strict_range,
-                                # values=[-100,100])
+    offset = Channel.control(
+        "SENS{ch}:CORR:LOSS2?",
+        "SENS{ch}:CORR:LOSS2 %e",
+        """
+        Control the offset subtracted (in dB) from the power sensor measurement
+        to account for cable and other losses to the power meter. (float)
+        """,
+        cast=float,
+        validator=strict_range,
+        values=[-100, 100],
+    )
 
+    offset_enabled = Channel.control(
+        "SENS{ch}:CORR:LOSS2:STAT?",
+        "SENS{ch}:CORR:LOSS2:STAT %d",
+        """
+        Control whether the offset is enabled (bool)
+        """,
+        cast=bool,
+    )
 
-    offset_enabled = Channel.control('SENS{ch}:CORR:LOSS2:STAT?',
-                                'SENS{ch}:CORR:LOSS2:STAT %d',
-                                """
-                                Control whether the offset is enabled (bool)
-                                """,
-                                cast=bool,)
+    power = Channel.measurement(
+        "fetc{ch}?",
+        """
+        Measure the power at the power sensor. (float)
+        """,
+        cast=float,
+    )
+
+    # average_count = None
+    # averaging_enable = None
+    # auto_averaging = None
+    # filter = None
+    # filter_enabled = None
+    # filter_mode = None
+    # trigger_auto_delay = None
+    # trigger_source = None
+    # trigger_immediate = None
+    # trigger_continuous = None
+    # read = None
+    # measure = None
+    # resolution = None
+    # zero = None
+    # calibrate = None
+    # power_reference_enabled = None # Enable/Disable
+    # step_determination_enabled = None
+    # relative_offset = None
+    # relative_offset_enabled = None
+    # upper_window_unit = Instrument.control('UNIT1:POW?',
+    #                                        'UNIT1:POW %s',
+    #                                        """
+    #                                        Control the measurement units for the upper window. (string)
+    #                                        """,
+    #                                        cast=str,
+
+    #                                 )
+    # lower_window_unit = Instrument.control('UNIT2:POW?',
+    #                                        'UNIT2:POW %s',
+    #                                        """
+    #                                        Control the measurement units for the lower window. (string)
+    #                                        """,
+    #                                        cast=str,
+    #                                 )
+
 
 class AgilentE4418B(Instrument):
     """
@@ -109,7 +150,6 @@ class AgilentE4418B(Instrument):
         self._fw = ""
         self._sn = ""
         self._options = ""
-
 
         if name is None:
             # written this way to pass 'test_all_instruments.py' while allowing the
@@ -153,60 +193,13 @@ class AgilentE4418B(Instrument):
             self._manu, self._model, _, self._fw = self.id
         return self._fw
 
-
-    # offset = Instrument.control('CORR:LOSS2?',
-    #                             'CORR:LOSS2 %e',
-    #                             """
-    #                             Control the offset applied (in dB) to the power sensor measurement
-    #                             to account for cable and other losses to the power meter. (float) 
-    #                             """,
-    #                             cast=float,
-    #                             validator=strict_range,
-    #                             values=[-100,100])
-
-    # enable_offset = Instrument.control('CORR:LOSS2:STAT?',
-    #                                    'CORR:LOSS2:STAT %s',
-    #                                    """
-    #                                    Control the offset applied (in dB) to the power sensor measurement
-    #                                    to account for cable and other losses to the power meter. (float) 
-    #                                    """,
-    #                                    cast=bool,
-    #                                    values={True: 'ON',
-    #                                            False: 'OFF'}
-    #                                    )
-
-    # average_count = None
-    # averaging_enable = None
-    # auto_averaging = None
-    # filter = None
-    # filter_enabled = None
-    # filter_mode = None
-    # trigger_auto_delay = None
-    # trigger_source = None
-    # trigger_immediate = None
-    # trigger_continuous = None
-    # read = None
-    # measure = None
-    # resolution = None
-    # zero = None
-    # calibrate = None
-    # power_reference_enabled = None # Enable/Disable
-    # step_determination_enabled = None
-    # relative_offset = None
-    # relative_offset_enabled = None
-    # upper_window_unit = Instrument.control('UNIT1:POW?',
-    #                                        'UNIT1:POW %s',
-    #                                        """
-    #                                        Control the measurement units for the upper window. (string)
-    #                                        """,
-    #                                        cast=str,
-
-    #                                 )
-    # lower_window_unit = Instrument.control('UNIT2:POW?',
-    #                                        'UNIT2:POW %s',
-    #                                        """
-    #                                        Control the measurement units for the lower window. (string)
-    #                                        """,
-    #                                        cast=str,
-
-    #                                 )
+    power_reference_enabled = Instrument.control(
+        "OUTP:ROSC?",
+        "OUTP:ROSC %d",
+        """
+        Control the builtin reference power source 1mW @ 50 MHz.
+        """,
+        map_values=True,
+        values={True: 1, False: 0},
+        cast=int,
+    )
